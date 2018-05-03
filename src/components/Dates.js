59dd7e8868d8263b84216aa64e7f304e498494c4
serjt12 <jandreus87@hotmail.com>
2018-05-03 18:35:30 -0500

    final

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactCardFlip from 'react-card-flip'
 
 import frontDate1 from '../images/02.png'
 import backDate1 from '../images/02-2.png'
@@ -11,95 +10,25 @@ import frontDate4 from '../images/20.png'
 import backDate4 from '../images/20-2.png'
 
 class Dates extends Component {
-  state = {isFlipped: false};
-
-  handleClick = (e) => {
-    e.preventDefault()
-    this.setState({
-      isFlipped: !this.state.isFlipped
-    })
-  }
-  handleClick1 = (e) => {
-    e.preventDefault()
-    this.setState({
-      isFlipped1: !this.state.isFlipped1
-    })
-  }
-  handleClick2 = (e) => {
-    e.preventDefault()
-    this.setState({
-      isFlipped2: !this.state.isFlipped2
-    })
-  }
-  handleClick3 = (e) => {
-    e.preventDefault()
-    this.setState({
-      isFlipped3: !this.state.isFlipped3
-    })
-  }
   render () {
     return (
-      <div className='transition-container'>
-        <section>
-          <ReactCardFlip infinite isFlipped={this.state.isFlipped}>
-            <div key='front' className='date1'>
-              <button onClick={this.handleClick}>
-                <img src={frontDate1} alt='Funk Date' />
-              </button>
-            </div>
-            <div key='back' className='date1'>
-              <button onClick={this.handleClick}>
-                <img src={backDate1} alt='Funk Date' />
-              </button>
-            </div>
-          </ReactCardFlip>
-        </section>
-        <section>
-          <ReactCardFlip infinite isFlipped={this.state.isFlipped1}>
-            <div key='front' className='date2' >
-              <button onClick={this.handleClick1}>
-                <img src={frontDate2} alt='Funk Date' />
-              </button>
-            </div>
-
-            <div key='back' className='date2'>
-              <button onClick={this.handleClick1}>
-                <img src={backDate2} alt='Funk Date' />
-              </button>
-            </div>
-          </ReactCardFlip>
-        </section>
-        <section>
-          <ReactCardFlip infinite isFlipped={this.state.isFlipped2}>
-            <div key='front' className='date3' >
-              <button onClick={this.handleClick2}>
-                <img src={frontDate3} alt='Funk Date' />
-              </button>
-            </div>
-
-            <div key='back' className='date3'>
-              <button onClick={this.handleClick2}>
-                <img src={backDate3} alt='Funk Date' />
-              </button>
-            </div>
-          </ReactCardFlip>
-        </section>
-        <section>
-          <ReactCardFlip infinite isFlipped={this.state.isFlipped3}>
-            <div key='front' className='date4' >
-              <button onClick={this.handleClick3}>
-                <img src={frontDate4} alt='Funk Date' />
-              </button>
-            </div>
-
-            <div key='back' className='date4'>
-              <button onClick={this.handleClick3}>
-                <img src={backDate4} alt='Funk Date' />
-              </button>
-            </div>
-          </ReactCardFlip>
-        </section>
-
+      <div className='dates-container'>
+        <div>
+          <img alt='date 1' src={frontDate1} />
+          <img alt='back 1' src={backDate1} />
+        </div>
+        <div>
+          <img alt='date 2' src={frontDate2} />
+          <img alt='back 2' src={backDate2} />
+        </div>
+        <div>
+          <img alt='date 3' src={frontDate3} />
+          <img alt='back 3' src={backDate3} />
+        </div>
+        <div>
+          <img alt='date 4' src={frontDate4} />
+          <img alt='back 4' src={backDate4} />
+        </div>
       </div>
     )
   }
